515801536c2346c087efb414188bd291d263c2a7
xNOtme <tdhkcc7@gmail.com>
2018-12-04 20:35:32 +0700

    Boostrap -> Reactsrap

---
+++ build/precache-manifest.b720c1b29d64fdf2f217d585e69034ae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "1d18191ea26a28c7a203",
+    "url": "/static/css/main.62b6c178.chunk.css"
+  },
+  {
+    "revision": "1d18191ea26a28c7a203",
+    "url": "/static/js/main.1d18191e.chunk.js"
+  },
+  {
+    "revision": "3d7cd8bd370477a1d835",
+    "url": "/static/css/1.3922c656.chunk.css"
+  },
+  {
+    "revision": "3d7cd8bd370477a1d835",
+    "url": "/static/js/1.3d7cd8bd.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "2313f1644d4764a23aa80ee422cb5759",
+    "url": "/index.html"
+  }
+];
