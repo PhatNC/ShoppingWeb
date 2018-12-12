435bee2f1047c0f2dfdc42b41335acc027dae224
xNOtme <tdhkcc7@gmail.com>
2018-12-12 16:01:39 +0700

    responsive carousel, list product

--- src/components/CarouselBanner.js
+++ src/components/CarouselBanner.js
@@ -6,17 +6,18 @@ import {
   CarouselIndicators,
   CarouselCaption
 } from 'reactstrap';
+import '../styles/CarouselBanner.css';
 
 const items = [
   {
     src: 'https://www.sweetcake.co/media/wysiwyg/anniversary-banner_1.jpg',
-    altText: 'Slide 1',
-    caption: 'Slide 1'
+    altText: '',
+    caption: ''
   },
   {
     src: 'https://www.sweetcake.co/media/wysiwyg/new-year_1.jpg',
-    altText: 'Slide 2',
-    caption: 'Slide 2'
+    altText: '',
+    caption: ''
   },
   {
     src: 'https://www.sweetcake.co/media/wysiwyg/Birthday-banner_1.jpg',
--- src/components/ListProducts.js
+++ src/components/ListProducts.js
@@ -16,7 +16,7 @@ export default class ListProducts extends React.Component {
         <Row>
           <Col xs="12" sm="6" md="6" lg="3">
             <Card>
-              <CardImg top width="100%" src="https://www.sweetcake.co/media/catalog/product/cache/1/small_image/600x600/9df78eab33525d08d6e5fb8d27136e95/f/r/fruit_cake_7.jpg" alt="Card image cap" />
+              <CardImg style={{opacity: 0.5}} top width="100%" src="https://www.sweetcake.co/media/catalog/product/cache/1/small_image/600x600/9df78eab33525d08d6e5fb8d27136e95/f/r/fruit_cake_7.jpg" alt="Card image cap" />
               <CardBody>
                 <CardTitle>Fresh Fruit Cake</CardTitle>
                 <CardSubtitle>Birthday Cake</CardSubtitle>
@@ -54,7 +54,51 @@ export default class ListProducts extends React.Component {
               </CardBody>
             </Card>
           </Col>
+        </Row>        
+        <div class="clearfix">&nbsp;</div>
+        <Row>
+          <Col xs="12" sm="6" md="6" lg="3">
+            <Card>
+              <CardImg top width="100%" src="https://www.sweetcake.co/media/catalog/product/cache/1/small_image/600x600/9df78eab33525d08d6e5fb8d27136e95/b/a/basket_cake.jpg" alt="Card image cap" />
+              <CardBody>
+                <CardTitle>Fresh Fruit Cake</CardTitle>
+                <CardSubtitle>Birthday Cake</CardSubtitle>
+                <CardText>₹450</CardText>
+              </CardBody>
+            </Card>
+          </Col>
+          <Col xs="12" sm="6" md="6" lg="3">
+            <Card>
+              <CardImg top width="100%" src="https://www.sweetcake.co/media/catalog/product/cache/1/small_image/600x600/9df78eab33525d08d6e5fb8d27136e95/c/h/choco_strawerry_cake.jpg" alt="Card image cap" />
+              <CardBody>
+                <CardTitle>Fresh Fruit Cake</CardTitle>
+                <CardSubtitle>Birthday Cake</CardSubtitle>
+                <CardText>₹450</CardText>
+              </CardBody>
+            </Card>
+          </Col>
+          <Col xs="12" sm="6" md="6" lg="3">
+            <Card>
+              <CardImg top width="100%" src="https://www.sweetcake.co/media/catalog/product/cache/1/small_image/600x600/9df78eab33525d08d6e5fb8d27136e95/e/n/enchanted_flower.jpg" alt="Card image cap" />
+              <CardBody>
+                <CardTitle>Fresh Fruit Cake</CardTitle>
+                <CardSubtitle>Birthday Cake</CardSubtitle>
+                <CardText>₹450</CardText>
+              </CardBody>
+            </Card>
+          </Col>
+          <Col xs="12" sm="6" md="6" lg="3">
+            <Card>
+              <CardImg top width="100%" src="https://www.sweetcake.co/media/catalog/product/cache/1/small_image/600x600/9df78eab33525d08d6e5fb8d27136e95/f/r/frozen_photo_cake.jpg" alt="Card image cap" />
+              <CardBody>
+                <CardTitle>Fresh Fruit Cake</CardTitle>
+                <CardSubtitle>Birthday Cake</CardSubtitle>
+                <CardText>₹450</CardText>
+              </CardBody>
+            </Card>
+          </Col>
         </Row>
+
         </Container>
       );
     }
